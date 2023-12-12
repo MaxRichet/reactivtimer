4b16d57b1ff3cbb867452be1fc82f4d3794d0584
MaxRichet <max.richet@my-digital-school.org>
2023-12-12 16:56:57 +0100

    in progress

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://0.0.0.0:27017/timer');
+
+app.use(express.urlencoded());
+app.use(express.json());
+
+const userRoute = require(`./routes/userRoute`);
+
+userRoute(app);
+
+app.listen(port, () => {
+  console.log(`Exemple app listening on port ${port}`)
+});
