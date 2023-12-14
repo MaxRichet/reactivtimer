944d4cbd872f161961488c100614c8ab8f2e865f
MaxRichet <max.richet@my-digital-school.org>
2023-12-14 20:08:34 +0100

    swagger

--- src/app.js
+++ src/app.js
@@ -3,8 +3,13 @@ const app = express();
 const port = 3000;
 
 const mongoose = require('mongoose');
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpec = require('./docs/swagger-config.js');
 
 mongoose.connect('mongodb://0.0.0.0:27017/timer');
+
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use(express.urlencoded());
 app.use(express.json());
--- src/routes/timeRoute.js
+++ src/routes/timeRoute.js
@@ -1,7 +1,62 @@
+/**
+ * @openapi
+ * tags:
+ *   name: Time
+ *   description: Operations related to time entries
+ */
+
+/**
+ * @openapi
+ * /time:
+ *   post:
+ *     summary: Create a new time entry
+ *     description: Endpoint to create a new time entry.
+ *     tags: [Time]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               // Define your request body properties here
+ *     responses:
+ *       200:
+ *         description: Time entry created successfully
+ *         content:
+ *           application/json:
+ *             example: { message: 'Time entry created successfully' }
+ */
+
+/**
+ * @openapi
+ * /time:
+ *   get:
+ *     summary: Get a list of all time entries
+ *     description: Endpoint to get a list of all time entries.
+ *     tags: [Time]
+ *     responses:
+ *       200:
+ *         description: Successfully retrieved time entries
+ */
+
+/**
+ * @openapi
+ * /time/avg:
+ *   get:
+ *     summary: Get the average time
+ *     description: Endpoint to get the average time.
+ *     tags: [Time]
+ *     responses:
+ *       200:
+ *         description: Successfully retrieved average time
+ */
+
 module.exports = (server) => {
     const timeController = require('../controllers/timeController');
+    const jwtMiddlewares = require('../Middlewares/jwtMiddleware');
 
-    server.post('/time', timeController.createTime);
-    server.get('/time', timeController.listAllTime);
-    server.get('/time/avg', timeController.avgTime);
+    server.post('/time', jwtMiddlewares.verifyToken, timeController.createTime);
+    server.get('/time', jwtMiddlewares.verifyToken, timeController.listAllTime);
+    server.get('/time/avg', jwtMiddlewares.verifyToken, timeController.avgTime);
 }
